1f77862fd396e7952da9d2573c8e2f220f1b5961
John Wolfe <wolfejw86@gmail.com>
2020-06-05 11:15:28 -0400

    support and tests for key rotation via key as an array (#28)
    * support and tests for key rotation via key as an array
    
    * remove unnecessary comment
    
    * actually testing rotated keys with old session now
    
    * moved all requires to top of file
    
    * updating documentation
    
    * re-signing session with latest key if older signed session is encountered, additional docs update

---
+++ test/key-rotation.js
@@ -0,0 +1,150 @@
+'use strict'
+
+const Fastify = require('fastify')
+const tap = require('tap')
+const sodium = require('sodium-native')
+const fastifySecureSession = require('..')
+
+tap.test('support string key array', async t => {
+  const fastify = Fastify({ logger: false })
+
+  t.tearDown(fastify.close.bind(fastify))
+  t.plan(5)
+
+  const key1 = Buffer.alloc(sodium.crypto_secretbox_KEYBYTES)
+  sodium.randombytes_buf(key1)
+
+  const key2 = Buffer.alloc(sodium.crypto_secretbox_KEYBYTES)
+  sodium.randombytes_buf(key2)
+
+  fastify.register(fastifySecureSession, {
+    key: [key1.toString('base64'), key2.toString('base64')]
+  })
+
+  fastify.post('/', (request, reply) => {
+    request.session.set('data', request.body)
+    reply.send('hello world')
+  })
+
+  fastify.get('/', (request, reply) => {
+    const data = request.session.get('data')
+    if (!data) {
+      reply.code(404).send()
+      return
+    }
+    reply.send(data)
+  })
+
+  const payload = { some: 'data' }
+
+  const intialSetResponse = await fastify.inject({
+    url: '/',
+    method: 'POST',
+    payload
+  })
+
+  t.equal(intialSetResponse.statusCode, 200)
+  t.ok(intialSetResponse.headers['set-cookie'])
+
+  const getResponse = await fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      cookie: intialSetResponse.headers['set-cookie']
+    }
+  })
+
+  t.deepEqual(JSON.parse(getResponse.payload), payload)
+
+  const getResponseNewKey = await fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      cookie: intialSetResponse.headers['set-cookie']
+    }
+  })
+
+  t.equal(getResponseNewKey.statusCode, 200)
+  t.deepEqual(JSON.parse(getResponseNewKey.payload), payload)
+})
+
+tap.test('support key rotation with buffer key array', async t => {
+  let fastify = Fastify({ logger: false })
+
+  t.tearDown(fastify.close.bind(fastify))
+  t.plan(5)
+
+  const key1 = Buffer.alloc(sodium.crypto_secretbox_KEYBYTES)
+  sodium.randombytes_buf(key1)
+
+  const key2 = Buffer.alloc(sodium.crypto_secretbox_KEYBYTES)
+  sodium.randombytes_buf(key2)
+
+  fastify.register(fastifySecureSession, {
+    key: [key1, key2]
+  })
+
+  fastify.post('/', (request, reply) => {
+    request.session.set('data', request.body)
+    reply.send('hello world')
+  })
+
+  fastify.get('/', (request, reply) => {
+    const data = request.session.get('data')
+    if (!data) {
+      reply.code(404).send()
+      return
+    }
+    reply.send(data)
+  })
+
+  const payload = { some: 'data' }
+
+  const intialSetResponse = await fastify.inject({
+    url: '/',
+    method: 'POST',
+    payload
+  })
+
+  t.equal(intialSetResponse.statusCode, 200)
+  t.ok(intialSetResponse.headers['set-cookie'])
+
+  const getResponse = await fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      cookie: intialSetResponse.headers['set-cookie']
+    }
+  })
+
+  t.deepEqual(JSON.parse(getResponse.payload), payload)
+
+  // restart fastify to switch key order (rotation)
+  await fastify.close()
+
+  fastify = Fastify({ logger: false })
+
+  fastify.register(fastifySecureSession, {
+    key: [key2, key1]
+  })
+
+  fastify.get('/', (request, reply) => {
+    const data = request.session.get('data')
+    if (!data) {
+      reply.code(404).send()
+      return
+    }
+    reply.send(data)
+  })
+
+  const getResponseNewKey = await fastify.inject({
+    method: 'GET',
+    url: '/',
+    headers: {
+      cookie: intialSetResponse.headers['set-cookie']
+    }
+  })
+
+  t.equal(getResponseNewKey.statusCode, 200)
+  t.deepEqual(JSON.parse(getResponseNewKey.payload), payload)
+})
