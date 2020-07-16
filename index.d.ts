a3e8b53e068371a88bfde694bf3eef07c95f9599
Harry Brundage <harry.brundage@gmail.com>
2020-07-16 00:51:22 -0400

    Add TypeScript types
    Also bumps the dependency on fastify-cookie to a version who's types play nice with Fastify 3

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="node" />
+import { CookieSerializeOptions } from "fastify-cookie";
+import { FastifyPlugin } from "fastify";
+
+export interface Session {
+  changed: boolean;
+  deleted: boolean;
+  get(key: string): any;
+  set(key: string, value: any): void;
+  delete(): void;
+}
+
+export type SecureSessionPluginOptions = {
+  cookie?: CookieSerializeOptions
+} & ({key: string | Buffer | (string | Buffer)[]} | {
+  secret: string | Buffer,
+  salt: string | Buffer
+})
+
+declare const SecureSessionPlugin: FastifyPlugin<SecureSessionPluginOptions>;
+
+export default SecureSessionPlugin;
+
+declare module "fastify" {
+  interface FastifyRequest {
+    session: Session;
+  }
+}
