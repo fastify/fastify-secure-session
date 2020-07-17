5e07a41f7d33cc9a57b55b92bb46d20f9512921d
Harry Brundage <harry.brundage@gmail.com>
2020-07-17 07:01:21 -0400

    Add TypeScript types (#29)
    Also bumps the dependency on fastify-cookie to a version who's types play nice with Fastify 3

---
+++ test-d/index.test-d.ts
@@ -0,0 +1,24 @@
+import SecureSessionPlugin, { Session } from "..";
+import fastify, {
+  FastifyRequest,
+  FastifyInstance,
+  RequestGenericInterface,
+  FastifyReply,
+} from "fastify";
+import { expectType } from "tsd";
+
+const app: FastifyInstance = fastify();
+app.register(SecureSessionPlugin);
+app.register(SecureSessionPlugin, { key: "foobar" });
+app.register(SecureSessionPlugin, { key: Buffer.from("foo") });
+app.register(SecureSessionPlugin, { key: ["foo", "bar"] });
+app.register(SecureSessionPlugin, { secret: "foo", salt: "bar" });
+
+app.get("/not-websockets", async (request, reply) => {
+  expectType<FastifyRequest>(request);
+  expectType<FastifyReply>(reply);
+  expectType<Session>(request.session);
+  request.session.set("foo", "bar");
+  request.session.get("foo");
+  request.session.delete();
+});
