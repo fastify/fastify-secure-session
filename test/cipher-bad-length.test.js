c89b42970cdfd9205e804b77fc706fd8771f179e
Mohamed Edrah <mohamedsalemedrah@gmail.com>
2021-04-30 09:09:18 +0200

    Test if decoding an invalid cookie (signed with different key) fails

--- test/cipher-bad-length.test.js
+++ test/cipher-bad-length.test.js
@@ -38,3 +38,60 @@ test('decodeSecureSession should return null when the cipher stored in cookie is
     return `${cyphertextB64};${nonce}`
   }
 })
+
+test('decodeSecureSession should return null if cipher cannot be decrypted', async (t) => {
+  t.plan(2)
+
+  // Creating the keys
+  const firstKey = Buffer.allocUnsafe(sodium.crypto_secretbox_KEYBYTES)
+  const secondKey = Buffer.allocUnsafe(sodium.crypto_secretbox_KEYBYTES)
+
+  // Creating the initial instance to get a cookie
+  // signed by the first key
+  let app = createApp(firstKey)
+
+  const encryptedCookie = (await app.inject({ method: 'GET', url: '/' })).cookies[0].value
+
+  // Closing and recreating the app instance
+  await app.close()
+  app = createApp(secondKey)
+
+  // Running the assertions
+  await app.inject({
+    url: '/assert',
+    method: 'GET',
+    cookies: {
+      session: encryptedCookie
+    }
+  })
+
+  // ********************
+  function createApp (key) {
+    const Fastify = require('fastify')
+    const secureSession = require('..')
+    const app = Fastify({ logger: false })
+
+    app.register(secureSession, { key })
+
+    // Setting the session cookie
+    app.get('/', async function (req) {
+      req.session.set('greeting', { hello: 'world' })
+      return 'set!'
+    })
+
+    // Route that runs all of the test's assertions
+    app.get('/assert', async function (req) {
+      let logged = false
+      const decodedSession = this.decodeSecureSession(req.cookies.session, {
+        debug () { logged = true }
+      })
+
+      t.equal(decodedSession, null)
+      t.equal(logged, true)
+
+      return 'verified!'
+    })
+
+    return app
+  }
+})
