de4c52948f136c8987e2bea8e664b3883a95ba1c
Frazer Smith <frazer.dev@icloud.com>
2024-12-08 12:51:04 +0000

    build(deps-dev): replace standard with neostandard (#250)

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,21 +1,21 @@
-import SecureSessionPlugin, { Session, SessionData } from "..";
+import SecureSessionPlugin, { Session, SessionData } from '..'
 import fastify, {
   FastifyRequest,
   FastifyInstance,
   FastifyReply,
-} from "fastify";
-import { expectType } from "tsd";
+} from 'fastify'
+import { expectType } from 'tsd'
 
-const app: FastifyInstance = fastify();
-app.register(SecureSessionPlugin, { key: "foobar" });
-app.register(SecureSessionPlugin, { key: Buffer.from("foo") });
-app.register(SecureSessionPlugin, { key: ["foo", "bar"] });
-app.register(SecureSessionPlugin, { secret: "foo", salt: "bar" });
-app.register(SecureSessionPlugin, { sessionName: "foo", key: "bar" });
-app.register(SecureSessionPlugin, { expiry: 24 * 60 * 60, key: "bar" });
-app.register(SecureSessionPlugin, [{ sessionName: "foo", key: "bar" }, { sessionName: "bar", key: "bar" }]);
+const app: FastifyInstance = fastify()
+app.register(SecureSessionPlugin, { key: 'foobar' })
+app.register(SecureSessionPlugin, { key: Buffer.from('foo') })
+app.register(SecureSessionPlugin, { key: ['foo', 'bar'] })
+app.register(SecureSessionPlugin, { secret: 'foo', salt: 'bar' })
+app.register(SecureSessionPlugin, { sessionName: 'foo', key: 'bar' })
+app.register(SecureSessionPlugin, { expiry: 24 * 60 * 60, key: 'bar' })
+app.register(SecureSessionPlugin, [{ sessionName: 'foo', key: 'bar' }, { sessionName: 'bar', key: 'bar' }])
 
-declare module ".." {
+declare module '..' {
   interface SessionData {
     foo: string;
   }
@@ -25,43 +25,43 @@ interface FooSessionData {
   foo: string;
 }
 
-declare module "fastify" {
+declare module 'fastify' {
   interface FastifyRequest {
     foo: Session<FooSessionData>;
   }
 }
 
-app.get("/not-websockets", async (request, reply) => {
-  expectType<FastifyRequest>(request);
-  expectType<FastifyReply>(reply);
-  expectType<Session>(request.session);
-  request.session.set("foo", "bar");
-  expectType<string | undefined>(request.session.get("foo"));
-  expectType<any>(request.session.get("baz"));
-  expectType<string | undefined>(request.session.foo);
-  expectType<SessionData | undefined>(request.session.data());
-  request.session.delete();
+app.get('/not-websockets', async (request, reply) => {
+  expectType<FastifyRequest>(request)
+  expectType<FastifyReply>(reply)
+  expectType<Session>(request.session)
+  request.session.set('foo', 'bar')
+  expectType<string | undefined>(request.session.get('foo'))
+  expectType<any>(request.session.get('baz'))
+  expectType<string | undefined>(request.session.foo)
+  expectType<SessionData | undefined>(request.session.data())
+  request.session.delete()
   request.session.options({ maxAge: 42 })
-  request.session.touch();
+  request.session.touch()
 
-  request.foo.set("foo", "bar");
-  expectType<string | undefined>(request.foo.get("foo"));
-  expectType<any>(request.foo.get("baz"));
-  request.foo.delete();
-  request.foo.options({ maxAge: 42 });
-  request.foo.touch();
+  request.foo.set('foo', 'bar')
+  expectType<string | undefined>(request.foo.get('foo'))
+  expectType<any>(request.foo.get('baz'))
+  request.foo.delete()
+  request.foo.options({ maxAge: 42 })
+  request.foo.touch()
 
   // @ts-expect-error: set undefined key
-  request.session.set("baz", "bar");
+  request.session.set('baz', 'bar')
   // @ts-expect-error: invoke undefined key
-  expectType<any>(request.session.baz);
+  expectType<any>(request.session.baz)
   // @ts-expect-error: invoke undefined key
   request.baz.touch()
-});
+})
 
-expectType<Session | null>(app.decodeSecureSession("some cookie"))
-let session = app.createSecureSession({foo: 'bar'});
-expectType<Session>(session);
-session = app.createSecureSession();
-expectType<Session>(session);
+expectType<Session | null>(app.decodeSecureSession('some cookie'))
+let session = app.createSecureSession({ foo: 'bar' })
+expectType<Session>(session)
+session = app.createSecureSession()
+expectType<Session>(session)
 expectType<string>(app.encodeSecureSession(session))
